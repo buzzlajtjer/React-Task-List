6890d89a6569dfaf2462e26d820eb2ebab7c0e89
Chrisian <cbl.johannesson@gmail.com>
2019-08-05 13:12:12 +0200

    v.1.0.6

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="header-text">Daily Tasks</h1>
+        <h1 className="header-text">Daily Tasks {this.state.todos.length}</h1>
         <AddTodos handleAddTodo={this.handleAddTodo} />
         <Todos
           todos={this.state.todos}
